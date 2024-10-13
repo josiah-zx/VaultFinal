8edd0c28845ceee6a52948d37147fff0cb6320ff
Fabiang120 <117304307+Fabiang120@users.noreply.github.com>
2024-10-13 05:26:42 -0400

    Added some more css code to the login

--- vault/src/Components/LoginForm/LoginForm.jsx
+++ vault/src/Components/LoginForm/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './LoginForm.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 const LoginForm = () =>{
     return (
         <div className='wrapper'>
@@ -7,13 +9,19 @@ const LoginForm = () =>{
                 <h1>Login</h1>
                 <div className="input-box">
                     <input type="text" placeholder='Username' required />
+                    <FaUser className= 'icons' />
                 </div>
                 <div className="input-box">
                     <input type="text" placeholder='Password' required />
+                    <FaLock className= 'icons'/>
                 </div>
                 <div className="remember-forgot">
                     <label><input type="checkbox" /> Remember Me</label>
                     <a href="#">Forgot Password?</a>
+                </div>
+                <button type="submit">Login</button>
+                <div className="register-link">
+                    <p>Need an account?<a href="#">Register</a></p>
                 </div>
             </form>
         </div>
