892383d0caa589027d863c017bc8a48ffefc3a0d
yoaaronw <117215469+yoaaronw@users.noreply.github.com>
2024-10-17 14:15:33 -0400

    Updated login and registration pages, and when form is valid the user will be routed to home page

--- vault/src/App.js
+++ vault/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import LoginForm from './Components/LoginForm/LoginForm';
 import RegistrationForm from "./Components/RegistrationForm/RegistrationForm";
+import Home from "./Components/Home/Home";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<LoginForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/home" element={<Home />} />
       </Routes>
   );
 }
